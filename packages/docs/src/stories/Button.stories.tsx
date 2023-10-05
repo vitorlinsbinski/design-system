c07d11d254edf192b6e8b6cbb6d12d664b5006a9
Vitor Linsbinski de Oliveira <vitor.linsbinski@hotmail.com>
2023-10-05 16:20:16 -0400

    Button added to Storybook

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,10 +1,20 @@
 import type { StoryObj, Meta } from "@storybook/react";
 
-import { Button } from "@ignite-ui/react";
+import { Button, ButtonProps } from "@ignite-ui/react";
 
 export default {
   title: "Button",
   component: Button,
-} as Meta;
 
-export const Primary: StoryObj = {};
+  args: {
+    children: "Enviar",
+  },
+} as Meta<ButtonProps>;
+
+export const Primary: StoryObj<ButtonProps> = {};
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: "big",
+  },
+};
